968a718200bd5ea4834f194fe333fa8bf5ae5494
Anton <>
2023-03-31 08:41:35 +0200

    Add project files.

---
+++ Array Inlämningsuppgift 5.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] Meningar = { "Hej1 Hej1 Hej1 Hej1 Hej1", "Hej2 Hej2 Hej2", "Hej3 Hej3 Hej3 Hej3" }; 
+            
+            for(int i = 0; i < Meningar.Length; i++)
+            {
+                Console.WriteLine(Meningar[i] + "\n");
+            }
+
+        }
+    }
+}
